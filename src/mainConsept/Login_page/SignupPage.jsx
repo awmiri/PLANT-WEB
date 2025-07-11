552b41428e864bc9c130d731f7fadedbb3308801
awmiri <talasaz1382@gmail.com>
2025-07-11 15:30:15 +0330

    add show password on password and repassword inpute

--- src/mainConsept/Login_page/SignupPage.jsx
+++ src/mainConsept/Login_page/SignupPage.jsx
@@ -12,6 +12,10 @@ export default function SignupPage() {
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
     const [RePass, setRepass] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRePass, setShowRepass] = useState(false)
+    console.log(Password);
+
     return (
         <>
             <div className='w-full h-full flex flex-col items-center justify-center'>
@@ -39,9 +43,9 @@ export default function SignupPage() {
                                 <svg viewBox="0 0 23 23" fill="none" className={`size-6 ${!focusesPassword ? 'fill-customgray3' : 'fill-hightGreen'} transition`}>
                                     <path d="M19.4063 9.66667H11.7522C11.0005 7.53083 8.9655 6 6.573 6C3.53883 6 1.073 8.46583 1.073 11.5C1.073 14.5342 3.53883 17 6.573 17C8.9655 17 11.0005 15.4692 11.7522 13.3333H12.073L13.9063 15.1667L15.7397 13.3333L17.573 15.1667L21.2397 11.4633L19.4063 9.66667ZM6.573 14.25C5.0605 14.25 3.823 13.0125 3.823 11.5C3.823 9.9875 5.0605 8.75 6.573 8.75C8.0855 8.75 9.323 9.9875 9.323 11.5C9.323 13.0125 8.0855 14.25 6.573 14.25Z" />
                                 </svg>
-                                <input type="password" className='w-full outline-0 ' onFocus={() => { setFocusesPassword(true) }} onBlur={() => { if (Password.trim() === "") { setFocusesPassword(false) } }} value={Password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type={showPassword ? 'text' : 'password'} className='w-full outline-0 ' onFocus={() => { setFocusesPassword(true) }} onBlur={() => { if (Password.trim() === "") { setFocusesPassword(false) } }} value={Password} onChange={(e) => setPassword(e.target.value)} autocomplete />
                             </div>
-                            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onClick={() => { setShowPassword(prev => (!prev)) }}>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                             </svg>
 
@@ -77,9 +81,9 @@ export default function SignupPage() {
                                 <svg viewBox="0 0 23 23" fill="none" className={`size-6 ${!focusesRePass ? 'fill-customgray3' : 'fill-hightGreen'} transition`}>
                                     <path d="M19.4063 9.66667H11.7522C11.0005 7.53083 8.9655 6 6.573 6C3.53883 6 1.073 8.46583 1.073 11.5C1.073 14.5342 3.53883 17 6.573 17C8.9655 17 11.0005 15.4692 11.7522 13.3333H12.073L13.9063 15.1667L15.7397 13.3333L17.573 15.1667L21.2397 11.4633L19.4063 9.66667ZM6.573 14.25C5.0605 14.25 3.823 13.0125 3.823 11.5C3.823 9.9875 5.0605 8.75 6.573 8.75C8.0855 8.75 9.323 9.9875 9.323 11.5C9.323 13.0125 8.0855 14.25 6.573 14.25Z" />
                                 </svg>
-                                <input type="password" className='w-full outline-0 ' onFocus={() => { setFocusesRepass(true) }} onBlur={() => { if (RePass.trim() === "") { setFocusesRepass(false) } }} value={RePass} onChange={(e) => setRepass(e.target.value)} />
+                                <input type={showRePass ? 'text' : 'password'} className='w-full outline-0 ' onFocus={() => { setFocusesRepass(true) }} onBlur={() => { if (RePass.trim() === "") { setFocusesRepass(false) } }} value={RePass} onChange={(e) => setRepass(e.target.value)} />
                             </div>
-                            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" onClick={() => { setShowRepass(prev => (!prev)) }}>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                             </svg>
 
