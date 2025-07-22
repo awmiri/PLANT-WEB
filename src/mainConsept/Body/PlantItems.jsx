2505becc6b79e3828ca77317cc02c79e2586c176
awmiri <talasaz1382@gmail.com>
2025-07-23 02:47:38 +0330

    add url to the pj also add comment to the pj

--- src/mainConsept/Body/PlantItems.jsx
+++ src/mainConsept/Body/PlantItems.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router'
 import './PlantItems.css'
 
+// in generally this function lead to create items section
 export default function PlantItems({ data }) {
     return (
         <>
+            {/* //plants title */}
             <h2 className='font-vazirBold text-[22px] text-hightGreen'>{data[0].categoriFn}</h2>
+            {/* //plants content */}
             <div className='flex items-center justify-between gap-4 md:gap-6 shrink-0 overflow-x-auto scrollbar-hide mb-36 relative'>
+                {/* //get all of items from item obj*/}
                 {
                     data.map((item) => (
                         <div key={item.id} className='border border-customgray p-4 rounded-xl mt-6 min-w-[226px] md:min-w-[288px]'>
+                            {/* item img */}
                             <img src={item.img} alt="" />
+                            {/* // item price */}
                             <div className='w-full'>
                                 <h3 className='mt-6 font-vazirMediom text-lg text-customBlack'>{item.name}</h3>
                                 <div className='flex items-center justify-between mt-2.5'>
                                     <span className='text-sm font-vazirMediom text-customBlack'>قیمت:</span>
                                     <span className='text-customBlack font-vazirMediom'>{item.price.toLocaleString('fa-IR')} تومان</span>
                                 </div>
-                                <a href="" className='bg-hightGreen block rounded-lg mt-5 text-center text-white p-2 w-full'>مشاهده  بیشتر</a>
+                                {/* show information btn */}
+                                <Link to={`/iteminfo/${item.categoriEn}/${item.id}`} className='bg-hightGreen block rounded-lg mt-5 text-center text-white p-2 w-full'>مشاهده  بیشتر</Link>
                             </div>
                         </div>
                     ))
--- src/route.jsx
+++ src/route.jsx
@@ -5,14 +5,16 @@ import AuthLayout from './layouts/AuthLayout';
 import NotFound from './mainConsept/404err/NotFound'
 import UserProfile from "./mainConsept/UserProfile/UserProfile";
 import ShoppingCart from "./mainConsept/ShoppingCart/ShoppingCart";
+import ItemInfo from "./mainConsept/ItemInfo/ItemInfo";
 
 let allRout = [
     {
         path: '/', element: <MainLayout />, children: [
             { index: true, element: <HomePage /> },
-            { path: '/userprofile', element: <UserProfile /> },
-            { path: '/Shoppingcart', element: <ShoppingCart /> },
-            { path: '/*', element: <NotFound /> }
+            { path: 'userprofile', element: <UserProfile /> },
+            { path: 'Shoppingcart', element: <ShoppingCart /> },
+            { path: 'iteminfo/:category/:id', element: <ItemInfo /> },
+            { path: '*', element: <NotFound /> }
         ]
     },
     {
